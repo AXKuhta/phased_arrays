7937ffefdcdf0228f5c132a718d9b5d98dc12064
AXKuhta <11133304+AXKuhta@users.noreply.github.com>
2026-05-26 23:32:26 +0800

    mlp_synthesis.py: add pre-steering, add optimizer

--- mlp_synthesis.py
+++ mlp_synthesis.py
@@ -23,10 +23,24 @@ class PlanarArray(torch.nn.Module):
 
 	pts = np.dstack([m, n, z])[0]
 
-	# Array weights (ones or taper function)
-	#taper = np.hamming(10)
-	#w = np.outer(taper, taper).flatten()
-	w = np.ones(10*10)
+	# Pre-steering
+	t = 10/57.2
+	p = 10/57.2
+	v = -np.array([
+		np.sin(t)*np.cos(p),
+		np.sin(t)*np.sin(p),
+		np.cos(t)
+	])
+
+	# Tapering (optional)
+	taper = torch.tensor(np.hamming(10))
+	taper = torch.outer(taper, taper).flatten()
+
+	# Array weights
+	# Should be all 1s for beam up
+	# Warning: ensure the datatype is complex
+	w = torch.exp(torch.tensor(pts) @ v * 1j * kd) * taper
+	w.requires_grad = True
 
 	#
 	# phi           azimuth
@@ -36,48 +50,59 @@ class PlanarArray(torch.nn.Module):
 		pts = self.pts
 		kd = self.kd
 
-		t = 0/57.2
-		p = 0/57.2
-
 		vec = np.array([
 			np.sin(theta)*np.cos(phi),
 			np.sin(theta)*np.sin(phi),
 			np.cos(theta)
-		]) - np.array([
-			np.sin(t)*np.cos(p),
-			np.sin(t)*np.sin(p),
-			np.cos(t)
-		])[:, None]
+		])
+
+		vec = torch.tensor(vec + .0)
+		pts = torch.tensor(pts + .0)
 
 		# Lag distance in a direction (virtual, to a far field point)
 		lag = pts @ vec
 
-		radiators = self.w[:, None] * np.exp(1j * lag * kd)
+		radiators = self.w[:, None] * torch.exp(1j * lag * kd)
 
-		return np.sum(radiators, 0)
+		return torch.sum(radiators, 0)
 
 	def loss(self, af, mask_upper, mask_lower):
-		u_loss = np.linalg.norm(np.maximum(0, af - mask_upper))
-		l_loss = np.linalg.norm(np.minimum(0, af - mask_lower))
+		af = torch.abs(af)
+		u_loss = torch.linalg.norm(torch.maximum(torch.zeros_like(af), af - mask_upper))
+		l_loss = torch.linalg.norm(torch.minimum(torch.zeros_like(af), af - mask_lower))
 
 		return u_loss + l_loss
 
 model = PlanarArray()
 
 a = np.linspace(-180, +180, 360)
-b = np.linspace(0, +90, 180)
+b = np.linspace(0, +90, 90)
 u, v = np.meshgrid(a/57.2, b/57.2)
 grid = np.dstack([u.flatten(), v.flatten()])[0]
 
-mask_mainlobe = ((u>=-10/57.2)*(u<10/57.2)*(v>=30/57.2)*(v<50/57.2)).flatten()
+def spherical_to_vector(azi, inc):
+	x = np.sin(inc) * np.cos(azi)
+	y = np.sin(inc) * np.sin(azi)
+	z = np.cos(inc)
+	return np.stack([x, y, z]).T
+
+vectors = spherical_to_vector(*grid.T)
+mainlobe = spherical_to_vector(np.radians(10), np.radians(10))
+cos_angs = vectors @ mainlobe
+angs = np.degrees(np.arccos(cos_angs))
+mask_mainlobe = angs < 5
+
 mask_upper = 90*mask_mainlobe + 10
 mask_lower = 100*mask_mainlobe
 
-af = model.array_factor(*grid.T)
+grid = torch.tensor(grid)
+mask_upper = torch.tensor(mask_upper)
+mask_lower = torch.tensor(mask_lower)
 
 def picture_raw(phi, theta, af):
-	plt.pcolormesh(a, b, np.abs(af).reshape(180, 360))
-	plt.contour(a, b, np.abs(af).reshape(180, 360)>0.5*np.max(af, 0), levels=[0.5], colors='red', linewidths=2)
+	plt.pcolormesh(a, b, np.abs(af).reshape(90, 360))
+	plt.contour(a, b, np.abs(af).reshape(90, 360)>0.5*np.max(af, 0), levels=[0.5], colors='red', linewidths=2)
+	plt.gca().set_aspect("equal")
 	plt.ylabel("Inclination")
 	plt.xlabel("Azimuth")
 	plt.show()
@@ -98,5 +123,24 @@ def picture(phi, theta, af):
 	plt.xlabel("$\sin(\Theta)\sin(\phi)$")
 	plt.show()
 
-#picture_raw(a, b, af)
-picture(*grid.T, af)
+af = model.array_factor(*grid.T)
+#picture_raw(a, b, af.detach().numpy())
+picture(*grid.T.numpy(), af.detach().numpy())
+
+picture(*grid.T.numpy(), mask_mainlobe)
+#picture_raw(a, b, mask_mainlobe)
+
+for i in range(100):
+	af = model.array_factor(*grid.T)
+
+	optim = torch.optim.SGD([model.w])
+	#optim = torch.optim.Adam([model.w])
+	loss = model.loss(af, mask_upper, mask_lower)
+
+	optim.zero_grad()
+	loss.backward()
+	optim.step()
+
+	print(loss)
+
+picture(*grid.T.numpy(), af.detach().numpy())
